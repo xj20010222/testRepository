74487922b89a3d685908e35321767a9ad267b964
xj <2932600521@qq.com>
2020-11-18 15:37:19 +0800

    idea第一次提交

--- javademom/src/javademom/test/HelloWord.java
+++ javademom/src/javademom/test/HelloWord.java
@@ -1,9 +1,17 @@
 package javademom.test;
 
+import org.junit.Test;
+
 public class HelloWord {
 	
 	
 	public static void main(String[] args) {
+
 		System.out.println("hello word!!!!!");
+	}
+
+	@Test
+	public void fun1(){
+		System.out.println("1111111111111111111");
 	}
 }
